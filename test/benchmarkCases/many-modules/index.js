7096aaa2f081037f0cfc04d854c040401f3c2626
Tobias Koppers <tobias.koppers@googlemail.com>
2017-11-30 18:09:30 +0100

    WIP

--- test/benchmarkCases/many-modules/index.js
+++ test/benchmarkCases/many-modules/index.js
@@ -8,3 +8,48 @@ import "./a?6";
 import "./a?7";
 import "./a?8";
 import "./a?9";
+import "./a?10";
+import "./a?11";
+import "./a?12";
+import "./a?13";
+import "./a?14";
+
+
+
+
+
+import "./a?15";
+import "./a?16";
+import "./a?17";
+import "./a?18";
+import "./a?19";
+import "./a?20";
+import "./a?21";
+import "./a?22";
+import "./a?23";
+import "./a?24";
+import "./a?25";
+import "./a?26";
+import "./a?27";
+import "./a?28";
+import "./a?29";
+import "./a?30";
+import "./a?31";
+import "./a?32";
+import "./a?33";
+import "./a?34";
+import "./a?35";
+import "./a?36";
+import "./a?37";
+import "./a?38";
+import "./a?39";
+import "./a?40";
+import "./a?41";
+import "./a?42";
+import "./a?43";
+import "./a?44";
+import "./a?45";
+import "./a?46";
+import "./a?47";
+import "./a?48";
+import "./a?49";
